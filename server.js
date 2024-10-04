fdabcd9caff14fbe62f429875fc82e1e2c19baa8
Cfeitz99 <147496459+Cfeitz99@users.noreply.github.com>
2024-10-04 16:29:12 +0200

    Update server.js

--- server.js
+++ server.js
@@ -12,15 +12,15 @@ app.use((req, res, next) => {
 });
 
 app.get('/', async (req, res) => {
-  const { contact_id } = req.query;
+  const { deal_id } = req.query;  // Fetch deal_id from query params
 
-  // Trigger the webhook for every request with a contact_id
-  if (contact_id) {
+  // Trigger the webhook for every request with a deal_id
+  if (deal_id) {
     try {
-      await axios.post('https://hooks.zapier.com/hooks/catch/16510018/3p6zwhq/', { contact_id });
-      console.log(`Webhook triggered for contact_id ${contact_id}`);
+      await axios.post('https://hooks.zapier.com/hooks/catch/16510018/3p6zwhq/', { deal_id });
+      console.log(`Webhook triggered for deal_id ${deal_id}`);
     } catch (error) {
-      console.error(`Failed to trigger webhook for contact_id ${contact_id}: ${error}`);
+      console.error(`Failed to trigger webhook for deal_id ${deal_id}: ${error}`);
     }
   }
 
